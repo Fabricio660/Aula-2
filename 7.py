c32b90252d64c1eba6caa1b19f5779e586ab2a70
Fabricio660 <fabriciosilvadossantos931@gmail.com>
2025-07-11 21:55:12 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,9 @@
+#Variáveis são usadas para salvar
+#algo na memória do computador.
+# uso nome_variavel = expressão
+nome = 'Fabrico'
+sobrenome = 'santos'
+print('Nome : ',nome)
+print('sobrenome : ',sobrenome)
+concatenação = 'Fabricio'+' '+'santos'
+print(concatenação)
---
+++ 8.py
@@ -0,0 +1,10 @@
+# Entrada
+nome = 'Fabrico'
+idade = 29
+altura = 1.72
+peso = 79
+
+print('Nome :',nome,'\n Idade :',idade)
+# calculo
+imc = peso / altura**2
+print('Nome :',nome,'\n Idade :',idade,'\n imc =',imc)
